832c92230532fd5fd3a3f102aa1091477e38f33d
tonymj76 <demeyortj@gmail.com>
2020-06-08 23:22:44 +0100

    routes

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,10 @@
-const user=require('../controllers/user_controller')
-const exp=require('express')
-const appRoute=exp.Router()
+const exp = require('express');
+const user = require('../controllers/user_controller');
+
+const appRoute = exp.Router();
 
 appRoute.post('/api/v1/login/', user.login);
-//register new user route
+// register new user route
 appRoute.post('/api/v1/register/', user.create);
 
-module.exports=appRoute
+module.exports = appRoute;
