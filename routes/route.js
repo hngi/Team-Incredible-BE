ef485e5348704385f3b2e38aa9621bf2f7a21f42
tonymj76 <demeyortj@gmail.com>
2020-06-09 10:51:25 +0100

    add comment

--- routes/route.js
+++ routes/route.js
@@ -4,7 +4,6 @@ const user = require('../controllers/user_controller');
 const appRoute = exp.Router();
 
 appRoute.post('/login', user.login);
-// register new user route
 appRoute.post('/register', user.create);
 
 module.exports = appRoute;
