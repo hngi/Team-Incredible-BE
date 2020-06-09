29844459b60678d35620eeef602ddca4655fa139
tonymj76 <demeyortj@gmail.com>
2020-06-09 02:01:44 +0100

    hotfix

--- routes/route.js
+++ routes/route.js
@@ -3,8 +3,8 @@ const user = require('../controllers/user_controller');
 
 const appRoute = exp.Router();
 
-appRoute.post('/api/v1/login/', user.login);
+appRoute.post('/login', user.login);
 // register new user route
-appRoute.post('/api/v1/register/', user.create);
+appRoute.post('/register', user.create);
 
 module.exports = appRoute;
