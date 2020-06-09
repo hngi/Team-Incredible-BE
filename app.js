c3d2cf519283ccc7e6c2d5b668f13b8d4f0694d3
Miracle Gabriel <sixpathdev@gmailc.com>
2020-06-09 14:24:46 +0100

    Replaced body-parser middleware with the express.json()

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 // const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const mainRoute = require('./routes/route');
 
 app.use((req, res, next) => {
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 // app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static('public'));
 
 
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/', (req, res) => {
-  res.render('index', { varible: 'hello Guys' });
+  res.render('index', { variable: 'Hello Guys' });
 });
 
 app.use('/api/v1', mainRoute);
