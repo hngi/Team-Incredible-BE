1f5d7196a1fdc62bc3d28b3cf442fdda7729281a
Frost199 <emmaldini12@gmail.com>
2020-06-10 02:15:21 +0100

    test: fix server shutdown
    The server shutdown was implemented so as to close every ports after
    each test has been run, this helps to prevent the tests from running in
    infinite loops

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,8 +1,8 @@
 //require assert to confirm test cases
 const assert = require('assert');
-//require functions from app.js
-const app = require('../../app');
 
+//require functions from app.js
+let app;
 
 
 /*READ ME
@@ -14,20 +14,29 @@ cmd> npm run test
 Note! this test would not pass until the condition is satisfied, 
 modification may still be required. Ref (Gafar_01)*/
 
-describe('POST /login', ()=>{
-    //sample correct JSON data which is in database
-    it('should respond 200', (done)=>{
-        request(app)
-            .post('/api/v1/login')
-            .send({username:'team.incredible@gmail.com',password:'Incredible'})
-            .expect(200)
-            
-    });
-    //sample incorrect JSON data which is not in database
-    it('should respond with 404', (done)=>{
-        request(app)
-            .post('/api/v1/login')
-            .send({username:"incorrect@gmail.com", password:"Patch"})
-            .expect(404)
-    });
+describe('POST /login', () => {
+
+   beforeEach(() => {
+    app = require('../../server');
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  //sample correct JSON data which is in database
+  it('should respond 200', (done) => {
+    request(app)
+      .post('/api/v1/login')
+      .send({ username: 'team.incredible@gmail.com', password: 'Incredible' })
+      .expect(200);
+
+  });
+  //sample incorrect JSON data which is not in database
+  it('should respond with 404', (done) => {
+    request(app)
+      .post('/api/v1/login')
+      .send({ username: 'incorrect@gmail.com', password: 'Patch' })
+      .expect(404);
+  });
 });
