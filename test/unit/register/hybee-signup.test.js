1c9476a20aa706ccb336ed1dfbb3c6ca30f3c993
Ibrahim Adekunle <adefemi101@gmail.com>
2020-06-10 15:29:06 +0100

    test: updated the test script and package.json file

--- test/unit/register/hybee-signup.test.js
+++ test/unit/register/hybee-signup.test.js
@@ -229,3 +229,122 @@ describe('User API', () => {
     });
   });
 });
+
+const validateInput = (text, notEmpty, isNumber) => {
+  // Validate user input with two pre-defined rules
+  if (!text) {
+    return false;
+  }
+  if (notEmpty && text.trim().length === 0) {
+    return false;
+  }
+  if (isNumber && +text === NaN) {
+    return false;
+  }
+  return true;
+};
+
+const validateEmail = (email, notEmpty) => {
+  if (!email) {
+    return false;
+  }
+  if (notEmpty && email.trim().length === 0) {
+    return false;
+  }
+  return true;
+};
+
+const getField = (name) => {
+  // Returns output text
+  return `${name}`;
+};
+
+const validatePassword = () => {};
+
+// const checkField =
+
+const functions = {
+  checkName: (name) => {
+    if (!validateInput(name, true, false)) {
+      return false;
+    }
+
+    return `${getField(name)}`;
+  },
+
+  checkEmail: (email) => {
+    if (!validateEmail(email, true)) {
+      return false;
+    }
+
+    return `${getField(email)}`;
+  },
+
+  checkPassword: (password) => {
+    if (!validateInput(password, true, false)) {
+      return false;
+    }
+
+    return `${getField(password)}`;
+  },
+
+  confirmPassword: (passwordConfirm) => {
+    if (!validateInput(passwordConfirm, true, false)) {
+      return false;
+    }
+
+    return `${getField(passwordConfirm)}`;
+  },
+};
+
+describe('Signup Fields Tests', () => {
+  it('it should get the first name', (done) => {
+    const firstName = functions.checkName('Ibrahim');
+    firstName.should.be.a('string');
+    done();
+  });
+
+  it('it should get the last name', (done) => {
+    const lastName = functions.checkName('Adekunle');
+    lastName.should.be.a('string');
+    done();
+  });
+
+  it('it should get the phone number format 1', (done) => {
+    const phoneNumber = functions.checkName('+2348131180177');
+    phoneNumber.should.be.a('string');
+    phoneNumber.should.match(/^\+(?:[0-9] ?){6,14}[0-9]$/);
+    done();
+  });
+
+  it('it should get the phone number format 2', (done) => {
+    const phoneNumber = functions.checkName('08131180177');
+    phoneNumber.should.be.a('string');
+    phoneNumber.should.match(/^[0]\d{10}$/);
+    done();
+  });
+
+  it('it should get a valid email', (done) => {
+    const email = functions.checkEmail('adefemi101@gmail.com');
+    email.should.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    done();
+  });
+
+  const password = functions.checkPassword('olaWale_17#');
+  it('password should be 8 chars or more and must contain at least 1 lowercase, uppercase, numeric and special characters', (done) => {
+    password.should.match(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_!@#\$%\^&\*])(?=.{8,})/
+    );
+
+    done();
+  });
+
+  it('it should confirm password', (done) => {
+    const confirmPassword = functions.confirmPassword('olaWale_17#');
+    password.should.be.eq(confirmPassword);
+
+    done();
+  });
+});
