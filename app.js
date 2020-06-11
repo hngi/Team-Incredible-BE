662f24bfef2d261eb05782a9c116f6756468f4a2
Xemicolon <1vibenation@gmail.com>
2020-06-11 13:19:13 +0100

    Reverted sign up page design

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const appRoute = require("./routes/route");
+const appRoute = require("./routes/newRoutes");
 
 const app = express();
 // const cors = require('cors');
@@ -18,22 +17,6 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
-=======
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const appRoute = require('./routes/newroutes');
-
-
-const app = express();
-// const cors = require('cors');
-const mainRoute = require('./routes/route');
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
->>>>>>> 90500199c41818a7d219beaffa76d4ba54fff2ba
   next();
 });
 
@@ -41,17 +24,14 @@ app.use((req, res, next) => {
 
 app.use(cookieParser());
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // Registering and setting  the view engine
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use((req, res, next) => {
   if (!!req.cookies.auth) {
@@ -62,12 +42,7 @@ app.use((req, res, next) => {
   next();
 });
 
-<<<<<<< HEAD
 app.use("/api/v1", mainRoute);
-=======
-
-app.use('/api/v1', mainRoute);
->>>>>>> 90500199c41818a7d219beaffa76d4ba54fff2ba
 app.use(appRoute);
 
 module.exports = app;
