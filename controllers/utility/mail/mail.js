aaa211f49bf89bec4d0d4ff19a84854a905caa01
Ethical Ralph <rakinola90@gmail.com>
2020-06-13 09:26:32 +0100

    (Update): Changed mail stmp to mailgun

--- controllers/utility/mail/mail.js
+++ controllers/utility/mail/mail.js
@@ -1,16 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transport = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'Mailgun',
   auth: {
-    user: 'microapidev@gmail.com',
-    pass: 'micropass',
+    user: 'postmaster@trade.levitfx.com',
+    pass: '9331f0ea0be45818f3bb62efaf878215-8b34de1b-16fce93a',
   },
 });
 
 exports.sendMail = (to, subject, html) => {
   const mailOptions = {
-    from: 'microapidev@gmail.com',
+    from: 'Auth@microapi.dev',
     to,
     subject,
     html,
